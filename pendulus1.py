166c116b21e41c5697ea571c30ef84f483829494
nocive.science <nocive.science@gmail.com>
2024-02-23 01:11:22 -0300

    casi sacando el add_updater

--- pendulus1.py
+++ pendulus1.py
@@ -1,7 +1,9 @@
 from manim import *
+import numpy as np
+
 class PendulusGroup(VGroup):
     CONFIG={
-
+        'time': 0,
     }
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -13,16 +15,18 @@ class PendulusGroup(VGroup):
         return dot
     def put_line(self, dot):
         line=Line(self.get_center(), self.get_center()+DOWN*2)
-        dot.move_to(line.get_end())
+        dot.add_updater(lambda d: d.move_to(line.get_end()))
+        line.add_updater(self.update_pendulus)
         line.dot=dot
         return line
+    def update_pendulus(self, dt):
+        self.time += dt
+        angle_range = np.radians([120, 240])  # Oscilar entre el primer y cuarto cuadrante
+        angle = np.mean(angle_range) + np.diff(angle_range)/2 * np.sin(2 * np.pi * 0.5 * self.time)
+        self[1].set_angle(angle + PI / 2)
+
 class PendulusScene(Scene):
     def construct(self):
         pendulus=PendulusGroup()
-        pendulus.add_updater(self.update_pendulus)
         self.add(pendulus)
-        self.wait(3)
-    def update_pendulus(self, pendulus, dt):
-        pendulus.put_line(pendulus[1].dot)
-        pendulus[1].dot.move_to(pendulus[1].get_end())
-        return pendulus
+        self.wait(8)
