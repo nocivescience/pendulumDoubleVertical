57fdc07d2cc2f4e2df82f5cbd021929f10a119c5
nocive.science <nocive.science@gmail.com>
2024-02-23 02:10:46 -0300

    tambien funciona por los dos metodos

--- pendulus1.py
+++ pendulus1.py
@@ -10,22 +10,28 @@ class PendulusGroup(VGroup):
         self.time=self.CONFIG['time']
         self.derecha=True
         self.frequency=0.5
+        self.color=BLUE
         self.put_dot()
         self.put_line()
         self[1].add_updater(self.update_pendulus)
     def put_dot(self):
-        self.dot=Dot(color=BLUE).move_to(self.get_center())
+        self.dot=Dot(color=self.color).move_to(self.get_center())
         self.add(self.dot)
     def put_line(self):
         self.line=Line(self.get_center(), self.get_center()+DOWN*2)
         self.dot.add_updater(lambda d: d.move_to(self.line.get_end()))
         self.add(self.line)
     def update_pendulus(self, mob, dt):
-        self.time+=dt*self.frequency
+        direccion=1
         if self.derecha: 
             angle_range=np.radians([3, 177])
         else:
             angle_range=np.radians([357, 183])
+        if angle_range[0]<3:
+            direccion=1
+        elif angle_range[0]>357:
+            direccion=-1
+        self.time+=dt*self.frequency*direccion
         angle=np.mean(angle_range)+np.diff(angle_range)/2*np.sin(2*np.pi*0.5*self.time)
         mob.set_angle(angle+PI/2)
         self.dot.move_to(self.line.get_end())
