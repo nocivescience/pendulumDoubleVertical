41366f0b27584d8caa2b607b4d36a909497e75c1
nocive.science <nocive.science@gmail.com>
2024-02-23 03:06:53 -0300

    terminado

--- pendulus1.py
+++ pendulus1.py
@@ -1,5 +1,5 @@
 from manim import *
-import numpy as np
+import itertools as it
 
 class PendulusGroup(VGroup):
     CONFIG={
@@ -15,18 +15,18 @@ class PendulusGroup(VGroup):
         self.put_line()
         self[1].add_updater(self.update_pendulus)
     def put_dot(self):
-        self.dot=Dot(color=self.color).move_to(self.get_center())
+        self.dot=Dot(color=self.color, radius=.04).move_to(self.get_center())
         self.add(self.dot)
     def put_line(self):
-        self.line=Line(self.get_center(), self.get_center()+DOWN*2)
+        self.line=Line(self.get_center(), self.get_center()+DOWN*2, stroke_width=0.07)
         self.dot.add_updater(lambda d: d.move_to(self.line.get_end()))
         self.add(self.line)
     def update_pendulus(self, mob, dt):
         direccion=1
         if self.derecha: 
-            angle_range=np.radians([3, 177])
+            angle_range=np.radians([1, 179])
         else:
-            angle_range=np.radians([357, 183])
+            angle_range=np.radians([359, 181])
         if angle_range[0]<3:
             direccion=1
         elif angle_range[0]>357:
@@ -36,9 +36,20 @@ class PendulusGroup(VGroup):
         mob.set_angle(angle+PI/2)
         self.dot.move_to(self.line.get_end())
 class PendulusScene(Scene):
+    CONFIGURATION={
+        'colors': [BLUE, RED, GREEN, YELLOW, PURPLE, ORANGE, PINK, TEAL, WHITE, GREY]
+    }
     def construct(self):
-        pendulus1, pendulus2=PendulusGroup(), PendulusGroup()
-        pendulus1.derecha=True
-        pendulus2.derecha=False
-        self.add(pendulus1, pendulus2)
-        self.wait(8)
+        frequencies= [freq for freq in np.linspace(0.1, 1, 20)]
+        colors=it.cycle(self.CONFIGURATION['colors'])
+        pendulos=VGroup()
+        for side in [True, False]:
+            for freq in frequencies:
+                pendulus=PendulusGroup()
+                pendulus.derecha=side
+                pendulus.frequency=freq/4
+                pendulus[0].color=next(colors)
+                pendulus[0].set_z_index(-1)
+                pendulos.add(pendulus)
+        self.add(pendulos)
+        self.wait(90)
